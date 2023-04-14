669398a659048647229582c1275392f80d4177ae
sha-x-lil <shalil@whizzact.com>
2023-04-15 03:03:17 +0530

    form for chemmical and polyers in procurement page done

--- src/app/components/user/matrial-procurement/matrial-procurement.component.ts
+++ src/app/components/user/matrial-procurement/matrial-procurement.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare let $:any
 @Component({
   selector: 'app-matrial-procurement',
   templateUrl: './matrial-procurement.component.html',
@@ -7,18 +7,36 @@ import { Component } from '@angular/core';
 })
 export class MatrialProcurementComponent {
 
+
+chemicalPurity:any = '50';
+
+ngOnInit(): void {
+  $(document).ready(()=>{
+    $('#Procurement-matrial-1').addClass('active')
+  })
+}
+
+ShowMatrialForm(id:any){
+$('.Procurement-matrial').removeClass('active')
+$('#Procurement-matrial-'+id).addClass('active')
+$('.form').hide()
+$('#matrial-'+id).show()
+}
+
+setPurity(e:any){
+  this.chemicalPurity = e.target.value
+}
+
   categories = [
     {
       id: 1,
       name: "Metals",
       img: "../../../../assets/asserts/categories/metals.png",
-      url:""
     },
     {
       id: 2,
       name: "Chemicals",
       img: "../../../../assets/asserts/categories/chemicals.png",
-      url:""
     },
     {
       id: 3,
@@ -29,19 +47,16 @@ export class MatrialProcurementComponent {
       id: 4,
       name: "Composites",
       img: "../../../../assets/asserts/categories/composites.png",
-      url:""
     },
     {
       id: 5,
       name: "Ceramics",
       img: "../../../../assets/asserts/categories/ceramics.png",
-      url:""
     },
     {
       id: 6,
       name: "Natural Materials",
       img: "../../../../assets/asserts/categories/natural-material.png",
-      url:""
     },
   ]
 }

0110ec20fa227c221909d85c3c0af0718a719c0c
RUSHI <rushiaher02@gmail.com>
2023-04-15 11:18:19 +0530

    procurement page and project page done

--- src/app/components/user/dashboardion/dashboardion.component.ts
+++ src/app/components/user/dashboardion/dashboardion.component.ts
@@ -11,31 +11,25 @@ export class DashboardionComponent {
   projectData=[
     {
       id:'1',
-      name:'project 1',
+      name:'Helmet',
       date:'25 jan 2023',
-      summery:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      summary:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
   molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
   numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
   optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis`,
+  img:"../../../../assets/asserts/project assets/helmet.png"
     },
     {
       id:'2',
-      name:'project 2',
+      name:'Condenser',
       date:'15 feb 2023',
-      summery:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+      summary:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
   molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
   numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
   optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis`,
+       img:"../../../../assets/asserts/project assets/condenser.png"
     },
-    {
-      id:'3',
-      name:'project 3',
-      date:'14 march 2023',
-      summery:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-  molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-  numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-  optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis`,
-    },
+    
   ]
 
 
